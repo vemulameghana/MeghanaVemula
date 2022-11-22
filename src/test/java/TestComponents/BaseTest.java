29ad4d0a3f46d54c94112f339d5e31d67171e413
meghana <meghana.vemula@zemosolabs.com>
2022-11-22 16:12:18 +0530

    selenium code

--- src/test/java/TestComponents/BaseTest.java
+++ src/test/java/TestComponents/BaseTest.java
@@ -18,7 +18,7 @@ public class BaseTest {
 
     public static WebDriver InitializeDriver() throws IOException {
         Properties properties = new Properties();
-        FileInputStream fis = new FileInputStream("/home/meghav/Meghana/SeleniumWithTestNG/SeleniumWithFramework/src/test/java/frameworks/resources/Globaldata.properties");
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"/src/test/java/frameworks/resources/Globaldata.properties");
         properties.load(fis);
         String browserName = properties.getProperty("browser");
         if(browserName.equalsIgnoreCase("chrome")) {
